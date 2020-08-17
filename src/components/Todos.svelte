098e53c43bf8ca6277fffd7fa6d13fff1330e890
Xavi Clotet <xavi.clotet@credimarket.com>
2020-08-17 12:42:40 +0200

    more components

--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -3,7 +3,7 @@
   import { getContext } from "svelte";
   import TodoItem from "./TodoItem.svelte";
   import "firebase/firestore";
-
+  import { Textfield, Button, Icon } from "svelte-mui";
   // User ID passed from parent
   export let uid;
   const app = getContext("firebase").getFirebase();
@@ -58,17 +58,33 @@
   </ul>
 
   <div class="flex">
-
-    <input
+    <div class="w-40">
+      <Textfield
+        autocomplete="off"
+        label={placeholder}
+        required
+        on:keydown={onKeyDown}
+        bind:value={text} />
+    </div>
+    <div class="mt-4">
+      <Button color="primary" title="Add Task" on:click={add}>
+        Send
+        <!-- right icon -->
+        <Icon
+          path="M2.01 21L23 12 2.01 3 2 10l15 2-15 2z"
+          style="margin: 0 -4px 0 8px;" />
+      </Button>
+    </div>
+    <!--  <input
       class="flex-initial shadow appearance-none border rounded py-2 px-3
       text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
       id="task"
       bind:value={text}
       on:keydown={onKeyDown}
       {placeholder}
-      type="text" />
+      type="text" /> -->
 
-    <button
+    <!--  <button
       class="flex-initial mx-2 my-2 inline-flex items-center justify-center px-2
       py-1 text-base leading-5 rounded-md border font-medium shadow-sm
       transition ease-in-out duration-150 focus:outline-none
@@ -76,7 +92,7 @@
       hover:bg-green-500 hover:border-green-500 hover:text-gray-100"
       on:click={add}>
       Add Task
-    </button>
+    </button> -->
   </div>
 
 </Collection>
